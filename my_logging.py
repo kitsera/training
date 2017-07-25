58a9c5b8dbe1e411d5b48ebe759a4bb05a72d046
kitsera <sashaKitsera16358@gmail.com>
2017-07-25 18:43:47 +0300

    README ADDED

--- my_logging.py
+++ my_logging.py
@@ -6,3 +6,5 @@ LOG_FILENAME = 'my_log_file.log'
 logging.basicConfig(filename=LOG_FILENAME, format='%(asctime)s %(message)s', level=logging.DEBUG, filemode='w')
 
 logging.debug('msg')
+
+print('some changes')
